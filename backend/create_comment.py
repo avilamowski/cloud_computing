c5f28d2760cc397c75133f7886788bde4134c35a
avilamowski <abruvila@gmail.com>
2024-11-07 17:37:12 -0300

    Spam for comments

--- backend/create_comment.py
+++ backend/create_comment.py
@@ -4,46 +4,51 @@ import json
 import uuid
 import datetime
 import logging
+import boto3
+import os
+
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
+
+
+sqs_client = boto3.client("sqs")
+SQS_QUEUE_URL = os.environ.get("SQS_QUEUE_URL")
+
 
 def lambda_handler(event, context):
     session = get_session()
     logger.info("Event: %s", event)
 
     try:
-        data = json.loads(event['body'])
+        data = json.loads(event["body"])
 
-        content = data.get('content')
-        claims = event['requestContext']['authorizer']['claims']
-        email = claims['email']
+        content = data.get("content")
+        claims = event["requestContext"]["authorizer"]["claims"]
+        email = claims["email"]
 
-        publication_id = data.get('publication_id')
+        publication_id = data.get("publication_id")
 
         if not content or not email or not publication_id:
             return {
-                'statusCode': 400,
-                'body': json.dumps({'error': 'content, user_id, and publication_id are required'})
+                "statusCode": 400,
+                "body": json.dumps(
+                    {"error": "content, user_id, and publication_id are required"}
+                ),
             }
-            
+
         user = session.query(User).filter_by(email=email).first()
         if not user:
             logger.error("User not found")
-            return {
-                'statusCode': 400,
-                'body': 'User not found'
-            }
-                
+            return {"statusCode": 400, "body": "User not found"}
 
         new_comment = Comment(
             comment_id=str(uuid.uuid4()),
             content=content,
             user_id=user.user_id,
             publication_id=publication_id,
-            created_at=datetime.datetime.now()
+            created_at=datetime.datetime.now(),
         )
-        
 
         session.add(new_comment)
         session.commit()
@@ -51,19 +56,24 @@ def lambda_handler(event, context):
         new_comment_id = str(new_comment.comment_id)
         session.close()
 
-        logger.info(f'Comment: {new_comment_id}')
+        logger.info(f"Comment: {new_comment_id}")
+
+        sqs_message = {"id": new_comment_id, "type": "comment"}
+
+        sqs_client.send_message(
+            QueueUrl=SQS_QUEUE_URL, MessageBody=json.dumps(sqs_message)
+        )
 
         return {
-            'statusCode': 201,
-            'body': json.dumps({
-                'message': 'Comment created successfully',
-                'comment_id': new_comment_id
-            })
+            "statusCode": 201,
+            "body": json.dumps(
+                {
+                    "message": "Comment created successfully",
+                    "comment_id": new_comment_id,
+                }
+            ),
         }
 
     except Exception as e:
         session.close()
-        return {
-            'statusCode': 500,
-            'body': json.dumps({'error': str(e)})
-        }
+        return {"statusCode": 500, "body": json.dumps({"error": str(e)})}
